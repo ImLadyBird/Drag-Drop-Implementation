90ecb203b63aa2fcf6c6ab913d6815a8f071dd9e
Negin Momeni <neginmomeni214@gmail.com>
2025-08-26 19:57:36 +0330

    fix bug

--- JS/App.js
+++ JS/App.js
@@ -4,26 +4,31 @@ const listRight = document.querySelector(".list-right");
 const listLeft = document.querySelector(".list-left");
 const card = document.querySelectorAll(".card");
 
+let selected = null; 
+
 card.forEach((card) => {
   card.addEventListener("dragstart", function (e) {
-    let selected = e.target;
-
-    rightBox.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
-
-    rightBox.addEventListener("drop", function (e) {
-      listLeft.append(selected);
-      selected = "";
-    });
-
-    leftBox.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
-
-    leftBox.addEventListener("drop", function (e) {
-      listRight.append(selected);
-      selected = "";
-    });
+  selected = e.target;
   });
 });
+
+leftBox.addEventListener("dragover", function (e) {
+e.preventDefault();
+});
+
+leftBox.addEventListener("drop", function (e) {
+listLeft.append(selected);
+selected = null;
+});
+
+rightBox.addEventListener("dragover", function (e) {
+e.preventDefault();
+});
+
+rightBox.addEventListener("drop", function (e) {
+listRight.append(selected);
+selected = null;
+});
+
+
+    
